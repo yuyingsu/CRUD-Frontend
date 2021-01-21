cc85b4f01300c68707007e60fd44c5fd5f7b1ffd
ErickLojano <erickjlojano@gmail.com>
2021-01-21 16:42:19 -0500

    all campuses, has an error

--- src/components/routes/RoutesView.jsx
+++ src/components/routes/RoutesView.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { AllPlayersContainer } from '../containers';
+// import { AllPlayersContainer } from '../containers';
+import {AllCampusesContainer} from '../containers'
 
 const RoutesView = () => {
   return (
     <Switch>
-      <Route exact path="/" component={AllPlayersContainer} />
+      <Route exact path="/" component={AllCampusesContainer} />
     </Switch>
   )
 }
